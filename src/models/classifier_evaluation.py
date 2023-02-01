77702b0f3a350dc8ba7e69c40f1583f357421a22
anonymousesubmission <113857639+anonymousesubmission@users.noreply.github.com>
2023-01-31 18:04:27 -0600

    Add files via upload

---
+++ src/models/classifier_evaluation.py
@@ -0,0 +1,129 @@
+" utility preserving neural network"
+" This NN takes as inputs the learnt representation (r) and tries to infer the the private attribute (u)"
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import math
+
+
+class Classifier_image_cifar_evaluation(nn.Module):
+    def __init__(self,args):
+        super(Classifier_image_cifar_evaluation, self).__init__()
+        self.convnet = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fcnet = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+        )
+
+        # initialize weight
+        self._initialize_weights()
+
+    def forward(self, r):
+        # learnt representation r
+        out = self.convnet(r)
+        out = out.view(out.size(0), -1)
+        out = self.fcnet(out)
+
+        return out
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n)) #mean 0 and std 1 normalization
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+class Classifier_loans(nn.Module):
+    def __init__(self, args):
+        super(Classifier_loans, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.Dropout(),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.Dropout(),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 4),
+            nn.BatchNorm1d(4),
+            nn.Dropout(),
+            nn.ReLU(inplace=True),
+            nn.Linear(4, 3),
+        )
+    def forward(self, r):
+        # learnt representation r
+        out = self.net(r)
+        return out
+
+class Classifier_adult(nn.Module):
+    def __init__(self, args):
+        super(Classifier_adult, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.Dropout(),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.Dropout(),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 4),
+            nn.BatchNorm1d(4),
+            nn.Dropout(),
+            nn.ReLU(inplace=True),
+            nn.Linear(4, 2),
+            )
+
+    def forward(self, r):
+        # learnt representation r
+        out = self.net(r)
+        return out
